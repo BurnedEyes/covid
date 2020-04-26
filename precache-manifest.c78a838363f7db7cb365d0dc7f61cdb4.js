9903adc8fd6ab89d74c5836af4c26b88d1b8eecc
burnedEyes <jaroslaw.blazkow@gmail.com>
2020-04-26 14:45:01 +0200

    Updates

---
+++ precache-manifest.c78a838363f7db7cb365d0dc7f61cdb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5c2783133a6ace5c6b805926c48c7a0",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "156bb6dd9689e8f99878",
+    "url": "/covid/static/css/main.0f76dfc6.chunk.css"
+  },
+  {
+    "revision": "5a9b6eb33703af570f23",
+    "url": "/covid/static/js/2.633dfc0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid/static/js/2.633dfc0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "156bb6dd9689e8f99878",
+    "url": "/covid/static/js/main.19eadb3e.chunk.js"
+  },
+  {
+    "revision": "fcb8528b993ca6e63f74",
+    "url": "/covid/static/js/runtime-main.a3cc5d1e.js"
+  }
+]);
